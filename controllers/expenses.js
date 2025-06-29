cd44021bb1e22ca9b7f4146bad64501bf406cb7c
mkozhevnikov2323 <mkozhevnikov23@gmail.com>
2025-06-29 15:19:15 +0400

    fix routes and controllers for update and delete expenses

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -26,3 +26,48 @@ module.exports.addExpense = (req, res, next) => {
       return next(err);
     });
 };
+
+module.exports.updateExpense = (req, res, next) => {
+  const expenseId = req.params.id;
+  const userId = req.user._id;
+  const { amount, date, title, recipient, category, comment } = req.body;
+
+  Expense.findOneAndUpdate(
+    { _id: expenseId, owner: userId },
+    { amount, date, title, recipient, category, comment },
+    { new: true, runValidators: true },
+  )
+    .then((expense) => {
+      if (!expense) {
+        return res.status(404).send({ message: 'Расход не найден' });
+      }
+      res.status(200).send(expense);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Невалидные данные для обновления расхода' });
+      }
+      next(err);
+    });
+};
+
+module.exports.deleteExpense = (req, res, next) => {
+  const expenseId = req.params.id;
+  const userId = req.user._id;
+
+  Expense.findOneAndDelete({ _id: expenseId, owner: userId })
+    .then((expense) => {
+      if (!expense) {
+        return res.status(404).send({ message: 'Расход не найден' });
+      }
+      res.status(200).send({ message: 'Расход успешно удалён' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Некорректный ID расхода' });
+      }
+      next(err);
+    });
+};
--- routes/expenses.js
+++ routes/expenses.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { getExpenses, addExpense } = require('../controllers/expenses');
+const {
+  getExpenses,
+  addExpense,
+  updateExpense,
+  deleteExpense,
+} = require('../controllers/expenses');
 
 router.get('/', getExpenses);
 
@@ -17,6 +22,34 @@ router.post(
     }),
   }),
   addExpense,
+);
+
+router.patch(
+  '/:id',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().hex().length(24),
+    }),
+    body: Joi.object().keys({
+      amount: Joi.number().min(0),
+      date: Joi.date(),
+      title: Joi.string(),
+      recipient: Joi.string(),
+      category: Joi.string(),
+      comment: Joi.string().allow('', null),
+    }),
+  }),
+  updateExpense,
+);
+
+router.delete(
+  '/:id',
+  celebrate({
+    params: Joi.object().keys({
+      id: Joi.string().hex().length(24),
+    }),
+  }),
+  deleteExpense,
 );
 
 module.exports = router;
